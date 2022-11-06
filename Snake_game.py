76b8e28be1f2fce40a785771186ed8855dc52e9f
Felipe da Rocha Ferreira <felipe179971@hotmail.com>
2022-11-06 16:52:37 -0300

    11/6/2022

--- Snake_game.py
+++ Snake_game.py
@@ -2,11 +2,13 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 #Inicializar todas as funcoes e bibliotecas do pygame
 pygame.init()
 #Criando tela#####################################################
 largura=640;altura=480
-x=largura/2;y=0 #controle do movimento do retangulo
+x=largura/2;y=altura/2 #controle do movimento do retangulo
+x_food=randint(40,600);y_food=randint(50,430) #posicao aleatoria da comida
 relogio=pygame.time.Clock() #velocidade do jogo via 'frames'
 tela=pygame.display.set_mode((largura,altura))#(largura,altura)
 #Titulo da tela
@@ -24,15 +26,35 @@ while True:
         if event.type==QUIT:
             pygame.quit()
             exit()
+        '''
+        #Reconhecendo ações do teclado (estatico)
+        if event.type==KEYDOWN:
+            if event.key==K_a or event.key==K_LEFT:#if 'a' go to left
+                x=x-20
+            if event.key==K_d or event.key==K_RIGHT:#if 'd', go to right
+                x=x+20
+            if event.key==K_w or event.key==K_UP:#if 'w', go up
+                y=y-20
+            if event.key==K_s or event.key==K_DOWN:#if 's', go down
+                y=y+20 '''
+        #Reconhecendo ações do teclado (pressionado)
+        if pygame.key.get_pressed()[K_a]:#if 'a' go to left
+            x=x-20
+        if pygame.key.get_pressed()[K_d]:#if 'd', go to right
+            x=x+20
+        if pygame.key.get_pressed()[K_w]:#if 'w', go up
+            y=y-20
+        if pygame.key.get_pressed()[K_s]:#if 's', go down
+            y=y+20
         #Desenhando os objetos
         ##Retangulo
-        pygame.draw.rect(tela,(232,36,36),(x,y,40,50)) #local,(cor),(posicao,largura,altura)
-        if y>=altura:
-            y=0
-        y=y+1
+        snake=pygame.draw.rect(tela,(232,36,36),(x,y,40,40)) #local,(cor),(posicao,largura,altura)
         ###Circulo
-        #pygame.draw.circle(tela,(42,72,223),(300,260),40) #local,(cor),(posicao,posicao),radio)
+        food=pygame.draw.circle(tela,(42,72,223),(x_food,y_food),20) #local,(cor),(posicao,posicao),radio)
         ###Linha
         #pygame.draw.line(tela,(223,211,42),(390,0),(390,600),1)#local,(cor),(inicio,fim x),(inicio,fim y),espessura
+        #Eat the food
+        if snake.colliderect(food):
+            x_food=randint(40,600);y_food=randint(50,430) #posicao aleatoria da comida
     #Atualizar a tela a cada loop principal
     pygame.display.update()
