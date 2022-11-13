8058df3b8e01e30444b4e9191c098abc995e0795
Felipe da Rocha Ferreira <felipe179971@hotmail.com>
2022-11-13 12:37:37 -0300

    11/13/2022

--- Snake_game.py
+++ Snake_game.py
@@ -7,17 +7,28 @@ from random import randint
 pygame.init()
 #Criando tela#####################################################
 largura=640;altura=480
-x=largura/2;y=altura/2 #controle do movimento do retangulo
+x_snake=int(largura/2);y_snake=int(altura/2) #controle do movimento do retangulo
 x_food=randint(40,600);y_food=randint(50,430) #posicao aleatoria da comida
 relogio=pygame.time.Clock() #velocidade do jogo via 'frames'
 tela=pygame.display.set_mode((largura,altura))#(largura,altura)
 fonte=pygame.font.SysFont('arial',35,True,False)#Texto com o placar
 pontos=0
+#Music game
+back_ground_music=pygame.mixer.music.load('Extra/background1.mp3')
+pygame.mixer.music.play(-1) #dando play (o -1 faza musica ficar em looping
+pygame.mixer.music.set_volume(0.3)
+eat_music=pygame.mixer.Sound('Extra/smw_swimming.wav') #obrigatorio ser .wav
 #Titulo da tela
 pygame.display.set_caption("Snake Game")
 #Icone do jogo
 img=pygame.image.load('Extra/Snake_ico_removebg.png')##
 pygame.display.set_icon(img)
+#Aumentando o tamanho da cobra
+lista_snake = []  # armazena todos os valroes (historico da cabeca)
+def aumenta_cobra(lista_snake):
+    for XeY in lista_snake:
+        pygame.draw.rect(tela,(9,170,0),(XeY[0][0],XeY[0][1],20,20))
+
 #Loop principal do jogo##########################################
 while True:
     #Loop de checagem dos eventos
@@ -43,24 +54,32 @@ while True:
                 y=y+20 '''
         #Reconhecendo ações do teclado (pressionado)
         if pygame.key.get_pressed()[K_a]:#if 'a' go to left
-            x=x-20
+            x_snake=x_snake-20
         if pygame.key.get_pressed()[K_d]:#if 'd', go to right
-            x=x+20
+            x_snake=x_snake+20
         if pygame.key.get_pressed()[K_w]:#if 'w', go up
-            y=y-20
+            y_snake=y_snake-20
         if pygame.key.get_pressed()[K_s]:#if 's', go down
-            y=y+20
+            y_snake=y_snake+20
         #Desenhando os objetos
         ##Retangulo
-        snake=pygame.draw.rect(tela,(232,36,36),(x,y,40,40)) #local,(cor),(posicao,largura,altura)
+        snake=pygame.draw.rect(tela,(9,170,0),(x_snake,y_snake,20,20)) #local,(cor),(posicao,largura,altura)
         ###Circulo
-        food=pygame.draw.circle(tela,(42,72,223),(x_food,y_food),20) #local,(cor),(posicao,posicao),radio)
+        food=pygame.draw.circle(tela,(42,72,223),(x_food,y_food),10) #local,(cor),(posicao,posicao),radio)
         ###Linha
         #pygame.draw.line(tela,(223,211,42),(390,0),(390,600),1)#local,(cor),(inicio,fim x),(inicio,fim y),espessura
         #Eat the food
         if snake.colliderect(food):
             x_food=randint(40,600);y_food=randint(50,430) #posicao aleatoria da comida
             pontos=pontos+1 #se comer, ganha pontos
+            eat_music.play()#barulho da comida
+        #Aumentando o tamanho da cobra
+        lista_cabeca=[]#Ela armazena a entrada, quando a cobra come
+        lista_cabeca.append((x_snake))
+        lista_cabeca.append((y_snake))
+        lista_snake.append([lista_cabeca])
+        aumenta_cobra(lista_snake)
+
     #imprimindo o placar
     tela.blit(texto_formatado,(400,15))
     #Atualizar a tela a cada loop principal
